4689875d12809fafaa2730a978f1ce02f7ceaaa6
Yossi Gil <yossi.gil@gmail.com>
2014-07-07 01:36:01 +0300

    This commits to another programming idiom: use overloading for exactly what it was made of -- to epxress the fact that the same operation is coducted on elements of different shapes. In Spartan programming, manny functions wor is to apply some minimal conversion of the arguments and the passing these as arguments of to a function with identical name.
    In this case, the chain of reductions is simple; only one step, but the
    chain of reductions can be much, much longer.

---
+++ src/org/spartan/As.java
@@ -0,0 +1,41 @@
+/** Part of the "Spartan Blog"; mutate the rest / but leave this line as is */
+package org.spartan;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * A collection of <code><b>static</b></code> functions whose most appropriate
+ * 'sedoco' class is {@link As}. *
+ * 
+ * @author Yossi Gil
+ * @since Jul 8, 2014
+ * 
+ */
+public enum As {
+	// No values in an 'enum' a name space for a collection of 'static' functions.
+	;
+	/**
+	 * Converts a value, which can be either a <code><b>null</b></code> or
+	 * references to valid instances, into a {@link NonNull}
+	 * 
+	 * @param $
+	 *          some value
+	 * @return the parameter, after bing to a non-null string.
+	 */
+	public static String nonNulString(@Nullable final Object $) {
+		return $ == null ? NULL : nonNulString($.toString());
+	}
+	/**
+	 * Converts a {@link String}, which can be either a <code><b>null</b></code>
+	 * or an actual String, into a {@link NonNull} String.
+	 * 
+	 * @param $
+	 *          some value
+	 * @return the parameter, after bing to a non-null string.
+	 */
+	public static String nonNulString(@Nullable final String $) {
+		return $ != null ? $ : NULL;
+	}
+	public static final String NULL = "(null)";
+}
