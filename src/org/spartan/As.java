5acbc15a35be263a3117d6c1be240b5588448805
Yossi Gil <yossi.gil@gmail.com>
2014-07-07 10:56:59 +0300

    Yet another programming idiom: when in great doubt, sort alpabetically. In this commit, methods in enum used as namespaces of not initimately related static functions are sorted alphabetically.

--- src/org/spartan/As.java
+++ src/org/spartan/As.java
@@ -21,6 +21,34 @@ public enum As {
 	// No values in an 'enum' a name space for a collection of 'static' functions.
 	;
 	/**
+	 * The string returned when 'conversion to string' is applied to a
+	 * <code><b>null</b></code> value.
+	 */
+	public static final String NULL = "(null)";
+	/**
+	 * Converts a sequence of literals into an array.
+	 * 
+	 * @param <T>
+	 *          some arbitrary type
+	 * @param $
+	 *          some sequence of values of the type parameter
+	 * @return the parameters, organized as an array with entries whose type is
+	 *         the type parameter
+	 */
+	@SafeVarargs public static <T> T[] array(final T... $) {
+		return $;
+	}
+	/**
+	 * Converts a boolean into a bit value
+	 * 
+	 * @param $
+	 *          some boolean value
+	 * @return 1 if the parameter is true, 0 otherwise
+	 */
+	public static int bit(final boolean b) {
+		return b ? 1 : 0;
+	}
+	/**
 	 * Converts a value, which can be either a <code><b>null</b></code> or
 	 * references to valid instances, into a {@link NonNull}
 	 * 
@@ -43,37 +71,10 @@ public enum As {
 		return $ != null ? $ : NULL;
 	}
 	/**
-	 * Converts a boolean into a bit value
-	 * 
-	 * @param $
-	 *          some boolean value
-	 * @return 1 if the parameter is true, 0 otherwise
-	 */
-	public static int bit(final boolean b) {
-		return b ? 1 : 0;
-	}
-	/**
-	 * Converts a sequence of literals into an array.
-	 * 
-	 * @param <T>
-	 *          some arbitrary type
-	 * @param $
-	 *          some sequence of values of the type parameter
-	 * @return the parameters, organized as an array with entries whose type is
-	 *         the type parameter
-	 */
-	@SafeVarargs public static <T> T[] array(final T... $) {
-		return $;
-	}
-	/**
-	 * The string returned when 'conversion to string' is applied to a <code><b>null</b></code> value. 
-	 */
-	public static final String NULL = "(null)";
-	/**
-	 * A static nested class hosting unit tests for the nesting class Unit test for
-	 * the containing class. Note the naming convention: a) names of test methods do not use
-	 * are not prefixed by "test". This prefix is redundant. b) test methods begin with the name of the
-	 * method they check.
+	 * A static nested class hosting unit tests for the nesting class Unit test
+	 * for the containing class. Note the naming convention: a) names of test
+	 * methods do not use are not prefixed by "test". This prefix is redundant. b)
+	 * test methods begin with the name of the method they check.
 	 * 
 	 * @author Yossi Gil
 	 * @since 2014-05-31
@@ -81,12 +82,11 @@ public enum As {
 	@FixMethodOrder(MethodSorters.NAME_ASCENDING)//
 	@SuppressWarnings("static-method")//
 	public static class TEST {
-    @Test public void asBitOfFalse() {
-      assertEquals(0, As.bit(false));
-    }
-
-    @Test public void asBitOfTrue() {
-      assertEquals(1, As.bit(true));
-    }
+		@Test public void asBitOfFalse() {
+			assertEquals(0, As.bit(false));
+		}
+		@Test public void asBitOfTrue() {
+			assertEquals(1, As.bit(true));
+		}
 	}
 }
--- src/org/spartan/_.java
+++ src/org/spartan/_.java
@@ -17,37 +17,36 @@ public enum _ {
 	// functions.
 	;
 	/**
-	 * Removes the @Nullable annotation present on the type of a value. This
-	 * function is mainly used to make <code><b>null</b></code<]> checkers happy.
-	 * <p>
-	 * The parameter is an instance of an arbitrary type, T. The hidden assumption
-	 * is that a @Nullable annotation is present on T. 
-	 * Thus, the parameter may be either 
-	 * <code><b>null</b></code<]>, or an actual instance of T.
-	 * <p>
-	 * The function returns the same instance it received as a parameter, except that
-	 * this instance is returned as an instance of the type T <i>without</i> the @Nullable 
-	 * annotation. Execution is aborted with an {@link AssertionError} if the parameter is null.
-	 * <p>
-	 * As it turns out, this function is a (slow) logical no-op, but still applicable to 
-	 * arguments of type T, where T does not have the @Nullable annotation present on it.
-	 *  <p>
-	 * For reasons related to the way non-nullability is managed in Java, the compiler will 
-	 * not warn you from doing applying this function to a {@link NonNull} type. However, 
-	 * there is absolutely no point in removing a @Nullable  annotation if the type that 
-	 * does not have it. Doing so a is plain clutter. Since the compiler cannot assist you,
-	 * you will have to be on the guard.
+	 * Counts the number of items in an {@link Iterable}.
 	 * 
 	 * @param <T>
 	 *          some arbitrary type
-	 * @param $
-	 *          an instance of the type parameter
-	 * @return its parameter, after verifying that it is not
-	 *         <code><b>null</b></code<]>
+	 * @param ts
+	 *          some iterable over items whose type is the type parameter
+	 * @return the number of items the given iterable yields.
 	 */
-	public static <T> T nonNull(final @Nullable T $) {
-		assert $ != null;
+	public static <T> int count(final @Nullable Iterable<T> ts) {
+		int $ = 0;
+		if (ts != null)
+			for (final @Nullable
+			T t : ts)
+				$ += As.bit(t != null);
 		return $;
+	}
+	/**
+	 * Determine whether a <code><b>null</b></code> is found in a sequence of
+	 * objects
+	 * 
+	 * @param os
+	 *          an unknown number of objects
+	 * @return <code><b>null</b></code> <i>iff</i> one of the parameters is
+	 *         <code><b>null</b></code>
+	 */
+	public static boolean hasNull(final Object... os) {
+		for (final Object o : os)
+			if (o == null)
+				return true;
+		return false;
 	}
 	/**
 	 * Computes the maximum of two or more integers.
@@ -82,34 +81,36 @@ public enum _ {
 		return $;
 	}
 	/**
-	 * Determine whether a <code><b>null</b></code> is found in a sequence of
-	 * objects
-	 * 
-	 * @param os
-	 *          an unknown number of objects
-	 * @return <code><b>null</b></code> <i>iff</i> one of the parameters is
-	 *         <code><b>null</b></code>
-	 */
-	public static boolean hasNull(final Object... os) {
-		for (final Object o : os)
-			if (o == null)
-				return true;
-		return false;
-	}
-	/**
-	 * Counts the number of items in an {@link Iterable}.
+	 * Removes the @Nullable annotation present on the type of a value. This
+	 * function is mainly used to make <code><b>null</b></code<]> checkers happy.
+	 * <p>
+	 * The parameter is an instance of an arbitrary type, T. The hidden assumption
+	 * is that a @Nullable annotation is present on T. 
+	 * Thus, the parameter may be either 
+	 * <code><b>null</b></code<]>, or an actual instance of T.
+	 * <p>
+	 * The function returns the same instance it received as a parameter, except that
+	 * this instance is returned as an instance of the type T <i>without</i> the @Nullable 
+	 * annotation. Execution is aborted with an {@link AssertionError} if the parameter is null.
+	 * <p>
+	 * As it turns out, this function is a (slow) logical no-op, but still applicable to 
+	 * arguments of type T, where T does not have the @Nullable annotation present on it.
+	 *  <p>
+	 * For reasons related to the way non-nullability is managed in Java, the compiler will 
+	 * not warn you from doing applying this function to a {@link NonNull} type. However, 
+	 * there is absolutely no point in removing a @Nullable  annotation if the type that 
+	 * does not have it. Doing so a is plain clutter. Since the compiler cannot assist you,
+	 * you will have to be on the guard.
 	 * 
 	 * @param <T>
 	 *          some arbitrary type
-	 * @param ts
-	 *          some iterable over items whose type is the type parameter
-	 * @return the number of items the given iterable yields.
+	 * @param $
+	 *          an instance of the type parameter
+	 * @return its parameter, after verifying that it is not
+	 *         <code><b>null</b></code<]>
 	 */
-	public static <T> int count(final @Nullable Iterable<T> ts) {
-		int $ = 0;
-		if (ts != null)
-			for (final @Nullable T t : ts)
-				$ += As.bit(t != null);
+	public static <T> T nonNull(final @Nullable T $) {
+		assert $ != null;
 		return $;
 	}
 }
