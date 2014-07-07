013afdede4903fc73549a5350554504be825fb01
Yossi Gil <yossi.gil@gmail.com>
2014-07-07 10:49:43 +0300

    Polish the documentation

--- src/org/spartan/_.java
+++ src/org/spartan/_.java
@@ -1,6 +1,7 @@
 /** Part of the "Spartan Blog"; mutate the rest / but leave this line as is */
 package org.spartan;
 
+import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
 
 /**
@@ -16,11 +17,12 @@ public enum _ {
 	// functions.
 	;
 	/**
-	 * This function is used to remove the @Nullable annotation placed on the type
-	 * of a value. It is used to make <code><b>null</b></code<]> checkers happy.
+	 * Removes the @Nullable annotation present on the type of a value. This
+	 * function is mainly used to make <code><b>null</b></code<]> checkers happy.
 	 * <p>
-	 * This function takes an instance of an arbitrary type, T (with the hidden assumption
-	 * that but such that a @Nullable annotation is present on T). Thus, the parameter may be either 
+	 * The parameter is an instance of an arbitrary type, T. The hidden assumption
+	 * is that a @Nullable annotation is present on T. 
+	 * Thus, the parameter may be either 
 	 * <code><b>null</b></code<]>, or an actual instance of T.
 	 * <p>
 	 * The function returns the same instance it received as a parameter, except that
@@ -29,6 +31,12 @@ public enum _ {
 	 * <p>
 	 * As it turns out, this function is a (slow) logical no-op, but still applicable to 
 	 * arguments of type T, where T does not have the @Nullable annotation present on it.
+	 *  <p>
+	 * For reasons related to the way non-nullability is managed in Java, the compiler will 
+	 * not warn you from doing applying this function to a {@link NonNull} type. However, 
+	 * there is absolutely no point in removing a @Nullable  annotation if the type that 
+	 * does not have it. Doing so a is plain clutter. Since the compiler cannot assist you,
+	 * you will have to be on the guard.
 	 * 
 	 * @param <T>
 	 *          some arbitrary type
@@ -79,7 +87,7 @@ public enum _ {
 	 * 
 	 * @param os
 	 *          an unknown number of objects
-	 * @return <code><b>null</b></code> <i>iff</i>one of the parameters is
+	 * @return <code><b>null</b></code> <i>iff</i> one of the parameters is
 	 *         <code><b>null</b></code>
 	 */
 	public static boolean hasNull(final Object... os) {
