1df4f300a257c2d4319e2c2ef93a8582eef09d18
Yossi Gil <yossi.gil@gmail.com>
2016-08-11 10:36:19 +0300

    Make this into a small eclipse project.

---
+++ Manuscripts/Spartanizations/Sources/A.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class A {
+	int i = 9;
+	int j;
+	ArrayList<Integer> al = new ArrayList<Integer>();
+	j=1000;
+}
---
+++ Manuscripts/Spartanizations/Sources/Test3.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import org.eclipse.jdt.core.dom.*;
+
+public class Test3 {
+  public static void main(final String args[]) { go(); }
+  private static void go() {
+    final CompilationUnit u = makeCompilationUnit();
+    u.accept(new ASTVisitor() {
+      final Set<String> $ = new HashSet<>();
+      @Override public boolean visit(final SimpleName ¢) {
+        note(¢);
+        return true;
+      }
+      @Override public boolean visit(final VariableDeclarationFragment ¢) {
+        collect(¢.getName());
+        return false; // do not continue to avoid usage info
+      }
+      void note(final SimpleName ¢) {
+        if ($.contains(¢.getIdentifier()))
+          System.out.println("Usage of '" + ¢ + "' at line " + u.getLineNumber(¢.getStartPosition()));
+      }
+      void collect(final SimpleName ¢) {
+        $.add(¢.getIdentifier());
+        System.out.println("Declaration of '" + ¢ + "' at line" + u.getLineNumber(¢.getStartPosition()));
+      }
+    });
+  }
+  private static CompilationUnit makeCompilationUnit() {
+    return (CompilationUnit) makeParser().createAST(null);
+  }
+  private static ASTParser makeParser() {
+    final ASTParser $ = ASTParser.newParser(AST.JLS3);
+    $.setSource("public class A { int i = 9;  \n int j; \n ArrayList<Integer> al = new ArrayList<Integer>();j=1000; }".toCharArray()); // parser.setSource("/*abc*/".toCharArray());
+    $.setKind(ASTParser.K_COMPILATION_UNIT);
+    return $;
+  }
+}
