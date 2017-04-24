767289267e6cb9176ca6c404b797198ab7da679a
Yossi Gil <yogi@cs.technion.ac.il>
2017-04-24 23:19:42 +0300

    sync

---
+++ src/il/org/spartan/tables/TableWriter.java
@@ -0,0 +1,84 @@
+package il.org.spartan.tables;
+
+import java.io.*;
+import java.util.*;
+
+import il.org.spartan.*;
+
+/** TODO Yossi Gil Document Class
+ * @author Yossi Gil
+ * @since 2016-12-25 */
+public class TableWriter implements Closeable {
+  /** Create a new instance, writing into a given named file
+   * @param currentFileName the name of the output file
+   * @throws IOException */
+  public TableWriter(final TableRenderer renderer, final String basePath) throws IOException {
+    this.renderer = renderer;
+    fileName = basePath.replaceAll("\\.[a-z0-9]*$", "") + "." + renderer.extension();
+    file = new File(fileName);
+    writer = new FileWriter(file);
+    write(renderer.beforeTable());
+  }
+
+  public void write(final String s) {
+    try {
+      writer.write(s);
+      writer.flush();
+    } catch (final IOException ¢) {
+      throw new RuntimeException(¢);
+    }
+  }
+
+  public final File file;
+  /** The name of the file into which records are written. */
+  public final String fileName;
+  public final OutputStreamWriter writer;
+  public final TableRenderer renderer;
+  private boolean shouldPrintHeader = true;
+  private boolean footerPrinted;
+
+  @Override public void close() {
+    try {
+      if (footerPrinted)
+        write(renderer.afterFooter());
+      write(renderer.afterTable());
+      writer.close();
+    } catch (final IOException ¢) {
+      throw new RuntimeException(¢);
+    }
+  }
+
+  public void write(final Map<String, Object> ¢) {
+    if (shouldPrintHeader) {
+      shouldPrintHeader = false;
+      writeHeader(¢);
+    }
+    writeData(¢);
+  }
+
+  public void writeFooter(final Map<String, Object> ¢) {
+    if (!footerPrinted) {
+      write(renderer.beforeFooter());
+      footerPrinted = true;
+    }
+    write(renderer.footerBegin() + separate.these(¢.values()).by(renderer.footerSeparator()) + renderer.footerEnd());
+  }
+
+  private void writeData(final Map<String, Object> ¢) {
+    write(renderer.recordBegin() + separate.these(¢.values()).by(renderer.recordSeparator()) + renderer.recordEnd());
+  }
+
+  private void writeHeader(final Map<String, Object> ¢) {
+    renderer.setHeaderCount(¢.size());
+    write(renderer.beforeHeader() + //
+        renderer.headerLineBegin() + writeHeaderInner(¢) + renderer.headerLineEnd() + //
+        renderer.afterHeader());
+  }
+
+  private String writeHeaderInner(final Map<String, Object> m) {
+    final Separator s = new Separator(renderer.headerSeparator());
+    final StringBuilder $ = new StringBuilder();
+    m.keySet().forEach(λ -> $.append(s).append(λ != null ? λ : renderer.nil()));
+    return $ + "";
+  }
+}
---
+++ test/il/org/spartan/tables/TableTest.java
@@ -0,0 +1,28 @@
+package il.org.spartan.tables;
+
+import static il.org.spartan.azzert.*;
+
+import org.junit.*;
+
+import il.org.spartan.*;
+import nano.ly.*;
+
+/** Unit test for {@link Table#classToNormalizedFileName(String)}
+ * @author Yossi Gil
+ * @since 2017-01-05 */
+@SuppressWarnings("static-method")
+public class TableTest {
+  @Test public void test() {
+    azzert.that(Table.classToNormalizedFileName("Table_NanosByCategories"), is("nanos-by-categories"));
+  }
+
+  @Test public void test2() {
+    final String[] components = cCamelCase.components("Table_NanosByCategories");
+    azzert.that(components, is(new String[] { "Table", "Nanos", "By", "Categories" }));
+    azzert.that(separate.these(lisp.rest(as.iterable(components))).by('-').toLowerCase(), is("nanos-by-categories"));
+  }
+
+  @Test public void testi1() {
+    azzert.that(separate.these(as.array("a", "b")).by('-'), is("a-b"));
+  }
+}
